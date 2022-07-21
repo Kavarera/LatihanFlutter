f4ee3ee6c53028d352a27dcbccf7c2d903dff2e7
Kavarera <rafli.030715@gmail.com>
2022-07-21 18:34:55 +0700

    Removing hero widget from detailpage. because the route is too fast.

--- LatihanAPIJson/apijson_rafli/lib/pages/detailproduct.dart
+++ LatihanAPIJson/apijson_rafli/lib/pages/detailproduct.dart
@@ -12,8 +12,31 @@ class DetailProduct extends StatelessWidget {
         child: Expanded(
           child: Column(
             children: [
-              Hero(tag: product.id.toString(),
-               child: Image.network(product.images.elementAt(0))),
+              Container(
+                height: 300,
+                child: Expanded(
+                  child: ListView.builder(
+                    itemCount: product.images.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context,index)=>Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(
+                        child: Image.network(product.images.elementAt(index),
+                        fit: BoxFit.cover,
+                        loadingBuilder: (BuildContext context,Widget child,ImageChunkEvent? loadingProgress){
+                          return loadingProgress==null ? child : Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes!=null 
+                              ? loadingProgress.cumulativeBytesLoaded/loadingProgress.expectedTotalBytes! 
+                              : null
+                            ),
+                          );
+                        },),
+                      ),
+                    )
+                  ),
+                ),
+              ),
               Container(
                 margin: EdgeInsets.only(left: 10,right: 10),
                 child: Row(
@@ -47,33 +70,6 @@ class DetailProduct extends StatelessWidget {
                   ),
                   SingleChildScrollView(child: Text(product.description,overflow: TextOverflow.clip,)),
                   ],
-                ),
-              ),
-              Container(
-                height: 300,
-                child: Expanded(
-                  child: ListView.builder(
-                    itemCount: product.images.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context,index)=>Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Container(
-                        padding: EdgeInsets.only(left: 10,right: 10),
-                        alignment: Alignment.center,
-                        child: Image.network(product.images.elementAt(index),
-                        fit: BoxFit.cover,
-                        loadingBuilder: (BuildContext context,Widget child,ImageChunkEvent? loadingProgress){
-                          return loadingProgress==null ? child : Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes!=null 
-                              ? loadingProgress.cumulativeBytesLoaded/loadingProgress.expectedTotalBytes! 
-                              : null
-                            ),
-                          );
-                        },),
-                      ),
-                    )
-                  ),
                 ),
               ),
             ],
